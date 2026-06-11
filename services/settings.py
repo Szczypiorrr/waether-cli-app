96859809286645053e13ef3d10f4ea723ebb76c7
Szczypiorrr <wintheer09@gmail.com>
2026-06-11 23:46:50 +0200

    feat: add settings management for default city and forecast days

--- services/settings.py
+++ services/settings.py
@@ -1,4 +1,5 @@
 import json
+from validator import validate_days, validate_city
 
 def load_default_city():
     with open("data/settings.json", "r") as f:
@@ -7,7 +8,17 @@ def load_default_city():
 
         return default_city
     
+def load_default_days():
+    with open("data/settings.json", "r") as f:
+        data = json.load(f)
+        default_days = data["days"]
+
+        return default_days
+
 def save_default_city(city):
+    if not validate_city(city):
+        return
+    
     with open("data/settings.json", "r") as f:
         data = json.load(f)
         
@@ -16,3 +27,15 @@ def save_default_city(city):
     with open("data/settings.json", "w") as f:
         json.dump(data, f, indent=4)
 
+
+def save_default_days(days):
+    if not validate_days(days):
+        return
+    
+    with open("data/settings.json", "r") as f:
+        data = json.load(f)
+        
+    data["days"] = days
+
+    with open("data/settings.json", "w") as f:
+        json.dump(data, f, indent=4)
