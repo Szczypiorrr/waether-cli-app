bd5751f6dd8a932418accab92c582a55ead3d867
Szczypiorrr <wintheer09@gmail.com>
2026-06-12 00:21:51 +0200

    refactor: handle missing settings.json in load/save functions

--- services/settings.py
+++ services/settings.py
@@ -2,26 +2,35 @@ import json
 from services.validator import validate_days, validate_city
 
 def load_default_city():
-    with open("data/settings.json", "r") as f:
-        data = json.load(f)
-        default_city = data["default_city"]
+    try:
+        with open("data/settings.json", "r") as f:
+            data = json.load(f)
+            default_city = data["default_city"]
 
-        return default_city
+            return default_city
+    except FileNotFoundError:
+        return None
     
 def load_default_days():
-    with open("data/settings.json", "r") as f:
-        data = json.load(f)
-        default_days = data["days"]
+    try:
+        with open("data/settings.json", "r") as f:
+            data = json.load(f)
+            default_days = data["days"]
 
-        return default_days
+            return default_days
+    except FileNotFoundError:
+        return None
 
 def save_default_city(city):
     if not validate_city(city):
         return
     
-    with open("data/settings.json", "r") as f:
-        data = json.load(f)
-        
+    try:
+        with open("data/settings.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        return None
+
     data["default_city"] = city
 
     with open("data/settings.json", "w") as f:
@@ -32,8 +41,11 @@ def save_default_days(days):
     if not validate_days(days):
         return
     
-    with open("data/settings.json", "r") as f:
-        data = json.load(f)
+    try:
+        with open("data/settings.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        return None
         
     data["days"] = days
 
